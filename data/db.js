f4e6b60f8ece8df2fe3bf0c9f7286d5774da0909
Larissa Miranda <90649436+laalnuquerque@users.noreply.github.com>
2021-12-09 14:33:46 -0300

    Add files via upload

---
+++ data/db.js
@@ -0,0 +1,34 @@
+let id = 5
+function proximoID() {
+    return id++
+}
+
+const perfis = [
+    { id: 1, nome: 'comum' },
+    { id: 2, nome: 'administrador' }
+]
+
+const usuarios = [{
+    id: proximoID,
+    nome: 'João Silva',
+    email: 'jsilva@zemail.com',
+    idade: 29,
+    perfil_id: 1,
+    status: 'ATIVO'
+}, {
+    id: proximoID,
+    nome: 'Rafael Junior',
+    email: 'rafajun@wemail.com',
+    idade: 31,
+    perfil_id: 2,
+    status: 'INATIVO'
+}, {
+    id: proximoID,
+    nome: 'Daniela Smith',
+    email: 'danismi@umail.com',
+    idade: 24,
+    perfil_id: 1,
+    status: 'BLOQUEADO'
+}]
+
+module.exports = { usuarios, perfis, proximoID }
